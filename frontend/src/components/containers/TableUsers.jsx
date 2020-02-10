cb979ed520e01f290368b04d8c627a4fae3c2423
kadrivaripuu <kadri.varipuu@gmail.com>
2020-02-10 17:47:21 +0200

    PHT003 All Users Delete & Edit functions (buggy)

--- frontend/src/components/containers/TableUsers.jsx
+++ frontend/src/components/containers/TableUsers.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import axios from 'axios';
 import httpsProxyAgent from 'https-proxy-agent';
+import ReactModal from 'react-modal';
 
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
@@ -16,90 +17,10 @@ import paginationFactory, { PaginationProvider, PaginationListStandalone, SizePe
 // import { usersData } from '../data/UsersData';
 const findUsersUrl = "http://localhost:8080/rest/user/all";
 const deleteUserUrl = "http://localhost:8080/rest/user/delete/";
+const oneUserUrl = "http://localhost:8080/rest/user/";
+const saveUserUrl = "http://localhost:8080/rest/user/save";
 
 const { SearchBar } = Search;
-
-const selectRow = {
-    mode: 'checkbox',
-    clickToSelect: true,
-};
-
-const userDetails = (e) => {
-    // console.log(e.target);
-    let { id } = e.target;
-
-    console.log("See Details for Id: " + id);
-    //hashHistory.push('/contacts/details/'+id);
-}
-
-const deleteUser = (e) => {
-    var agent = new httpsProxyAgent('http://kn.proxy.int.kn:80');
-        var config = {
-            httpsAgent: agent
-        }
-    let { id } = e.target;
-    axios.get(deleteUserUrl+id, config)
-    .then(data =>
-        this.setState({
-          isLoading: false,
-        }))
-    .catch((exception) => {
-        console.log(exception);
-    });
-}
-
-const formatProductDetailsButtonCell = (cell, row) => {
-    let clickHandler = deleteUser;
-    let emptyContent = React.createElement('i', { id: row.id });
-    let viewBtn = React.createElement('button', { id: row.id, className: "btnNtfcdDetails btn-action mdi mdi-account-remove btn-danger", onClick: clickHandler }, emptyContent);
-
-    return viewBtn;
-}
-
-const formatProductDetailsButtonCell2 = (cell, row) => {
-    let clickHandler = userDetails;
-    let emptyContent = React.createElement('i', { id: row.id });
-    let editBtn = React.createElement('button', { id: row.id, className: "btnNtfcdDetails btn-action mdi mdi-account-edit btn-warning", onClick: clickHandler }, emptyContent);
-    return editBtn;
-}
-
-const columns = [{
-    dataField: 'id',
-    text: 'ID',
-    sort: true
-}, {
-    dataField: 'fname',
-    text: 'First Name',
-    sort: true
-}, {
-    dataField: 'lname',
-    text: 'Last Name',
-    sort: true
-}, {
-    dataField: 'username',
-    text: 'Username',
-    sort: true
-}, {
-    dataField: 'password',
-    text: 'Password',
-    style: { color: "lightgray" }
-}, {
-    dataField: 'email',
-    text: 'Email',
-    sort: true
-}, {
-    dataField: 'role',
-    text: 'Role',
-    sort: true
-}, {
-    dataField: 'edit',
-    text: '[Edit]',
-    formatter: formatProductDetailsButtonCell2
-}, {
-    dataField: 'delete',
-    text: '[Delete]',
-    formatter: formatProductDetailsButtonCell
-}];
 
 const customTotal = (from, to, size) => (
     <span className="react-bootstrap-table-pagination-total">
@@ -136,24 +57,32 @@ const paginationConfig = {
 
 const rowEvents = {
     onClick: (e, row, rowIndex) => {
-      console.log(`clicked on row with index: ${rowIndex}`);
+        console.log(`clicked on row with index: ${rowIndex}`);
     },
-    
-  };
-  
+
+};
+
 
 
 
 export default class TableUsers extends React.Component {
     constructor(props) {
         super(props);
-        this.getDataFromDB = this.getDataFromDB.bind(this); 
+
         this.state = {
-            isLoading: true,
-            data: []
+            data: [],
+            id: "",
+            fname: "",
+            lname: "",
+            username: "",
+            password: "",
+            email: "",
+            role: "",
+            showModal: false
         };
 
-        
+        this.getDataFromDB = this.getDataFromDB.bind(this);
+
     }
 
 
@@ -167,14 +96,130 @@ export default class TableUsers extends React.Component {
             httpsAgent: agent
         }
         axios.get(findUsersUrl, config)
-        .then((response) => {
-            this.setState({data:response.data});
-        }).catch((exception) => {
-            console.log(exception);
-        });
+            .then((response) => {
+                this.setState({ data: response.data });
+            }).catch((exception) => {
+                console.log(exception);
+            });
     }
 
+
     render() {
+        const handleCloseModal = (e) => {
+            this.setState({ showModal: false });
+        }
+
+        const handleSave = (e) => {
+            const options = {
+                headers: { "Content-Type": "application/json", "Accept": "application/json" }
+            }
+            axios.post(saveUserUrl, this.state, options)
+                .then(
+                    this.setState({ showModal: false })
+                )
+                .then(
+                    this.getDataFromDB()
+                )
+                .then(
+                    this.getDataFromDB()
+                )
+                .catch((exception) => {
+                    console.log(exception);
+                });
+            e.preventDefault();
+
+        }
+
+        const getUserDetails = (e) => {
+            var agent = new httpsProxyAgent('http://kn.proxy.int.kn:80');
+            var config = {
+                httpsAgent: agent
+            }
+            let { id } = e.target;
+            axios.get(oneUserUrl + id, config)
+                .then((response) => {
+                    this.setState({ showModal: true })
+                    this.setState({ id: response.data.data.id });
+                    this.setState({ fname: response.data.data.fname });
+                    this.setState({ lname: response.data.data.lname });
+                    this.setState({ username: response.data.data.username });
+                    this.setState({ password: response.data.data.password });
+                    this.setState({ email: response.data.data.email });
+                    this.setState({ role: response.data.data.role });
+                })
+                .catch((exception) => {
+                    console.log(exception);
+                });
+        }
+
+        const deleteUser = (e) => {
+            var agent = new httpsProxyAgent('http://kn.proxy.int.kn:80');
+            var config = {
+                httpsAgent: agent
+            }
+            let { id } = e.target;
+            axios.get(deleteUserUrl + id, config)
+                // if I have time, figure out how to rerender after Delete...
+                .then(
+                    this.getDataFromDB()
+                )
+                .then(
+                    this.getDataFromDB()
+                )
+                .catch((exception) => {
+                    console.log(exception);
+                });
+        }
+
+        const formatProductDetailsButtonCell = (cell, row) => {
+            let emptyContent = React.createElement('i', { id: row.id });
+            let viewBtn = React.createElement('button', { id: row.id, className: "btnNtfcdDetails btn-action mdi mdi-account-remove btn-danger", onClick: deleteUser }, emptyContent);
+            return viewBtn;
+        }
+
+        const formatProductDetailsButtonCell2 = (cell, row) => {
+            let emptyContent = React.createElement('i', { id: row.id });
+            let editBtn = React.createElement('button', { id: row.id, className: "btnNtfcdDetails btn-action mdi mdi-account-edit btn-warning", onClick: getUserDetails }, emptyContent);
+            return editBtn;
+        }
+
+        const columns = [{
+            dataField: 'id',
+            text: 'ID',
+            sort: true
+        }, {
+            dataField: 'fname',
+            text: 'First Name',
+            sort: true
+        }, {
+            dataField: 'lname',
+            text: 'Last Name',
+            sort: true
+        }, {
+            dataField: 'username',
+            text: 'Username',
+            sort: true
+        }, {
+            dataField: 'password',
+            text: 'Password',
+            style: { color: "lightgray" }
+        }, {
+            dataField: 'email',
+            text: 'Email',
+            sort: true
+        }, {
+            dataField: 'role',
+            text: 'Role',
+            sort: true
+        }, {
+            dataField: 'edit',
+            text: '[Edit]',
+            formatter: formatProductDetailsButtonCell2
+        }, {
+            dataField: 'delete',
+            text: '[Delete]',
+            formatter: formatProductDetailsButtonCell
+        }];
 
         const contentTable = ({ paginationProps, paginationTableProps }) => {
             return (
@@ -182,9 +227,9 @@ export default class TableUsers extends React.Component {
                     <ToolkitProvider
                         keyField="id"
                         columns={columns}
-                        data={this.state.data}     
+                        data={this.state.data}
                         search
-                        
+
                         bootstrap4
                     >
                         {
@@ -206,8 +251,7 @@ export default class TableUsers extends React.Component {
                                             classes="table-responsive"
                                             striped
                                             hover
-                                            selectRow={selectRow}
-                                            rowEvents={ rowEvents }
+                                            rowEvents={rowEvents}
                                             {...toolkitprops.baseProps}
                                             {...paginationTableProps}
                                         />
@@ -216,9 +260,97 @@ export default class TableUsers extends React.Component {
                             }
                         }
                     </ToolkitProvider>
+                    <ReactModal
+                        isOpen={this.state.showModal}
+                        ariaHideApp={
+                            false}
+                        style={
+                            {
+                                overlay: {
+                                    position: 'fixed',
+                                    top: 65,
+                                    left: 220,
+                                    right: 20,
+                                    bottom: 100,
+                                    backgroundColor: 'rgba(255, 255, 255, 0.75)'
+                                },
+                                content: {
+                                    position: 'absolute',
+                                    top: '40px',
+                                    left: '40px',
+                                    right: '40px',
+                                    bottom: '40px',
+                                    border: '1px solid #ccc',
+                                    background: '#fff',
+                                    overflow: 'auto',
+                                    WebkitOverflowScrolling: 'touch',
+                                    borderRadius: '4px',
+                                    outline: 'none',
+                                    padding: '20px'
+                                }
+                            }
+                        }
+                    >
+
+                        <div className="row justify-content-around">
+                            <div className="col-md-8 ">
+                                <h4 className="card-title">{this.state.fname} {this.state.lname}</h4>
+                                <div className="form-group row">
+                                    <label htmlFor="fname" className="col-sm-4 text-right control-label col-form-label">First Name</label>
+                                    <div className="col-sm-8">
+                                        <input type="text" className="form-control" id="fname" placeholder="First Name Here..." value={this.state.fname} onChange={(e) => this.setState({ 'fname': e.target.value })} />
+                                    </div >
+                                </div>
+                                <div className="form-group row">
+                                    <label htmlFor="lname" className="col-sm-4 text-right control-label col-form-label">Last Name</label>
+                                    <div className="col-sm-8">
+                                        <input type="text" className="form-control" id="lname" placeholder="Last Name Here..." value={this.state.lname} onChange={(e) => this.setState({ 'lname': e.target.value })} />
+                                    </div >
+                                </div>
+                                <div className="form-group row">
+                                    <label htmlFor="username" className="col-sm-4 text-right control-label col-form-label">Username</label>
+                                    <div className="col-sm-8">
+                                        <input type="text" className="form-control" id="username" placeholder="Username Here..." value={this.state.username} onChange={(e) => this.setState({ 'username': e.target.value })} />
+                                    </div >
+                                </div>
+                                <div className="form-group row">
+                                    <label htmlFor="password" className="col-sm-4 text-right control-label col-form-label">Password</label>
+                                    <div className="col-sm-8">
+                                        <input type="text" className="form-control" id="password" placeholder="Password Here..." value={this.state.password} onChange={(e) => this.setState({ 'password': e.target.value })} />
+                                    </div >
+                                </div>
+                                <div className="form-group row">
+                                    <label htmlFor="email" className="col-sm-4 text-right control-label col-form-label">Email</label>
+                                    <div className="col-sm-8">
+                                        <input type="text" className="form-control" id="email" placeholder="Email Here..." value={this.state.email} onChange={(e) => this.setState({ 'email': e.target.value })} />
+                                    </div >
+                                </div>
+                                <div className="form-group row">
+                                    <label htmlFor="role" className="col-sm-4 text-right control-label col-form-label">Role</label>
+                                    <div className="col-sm-8">
+                                        <input type="text" className="form-control" id="role" placeholder="Enter Role(s) Here..." />
+                                    </div >
+                                </div>
+
+                                <div className="border-top row">
+                                    <div className="card-body">
+                                        <button type="submit" className="btn btn-primary" onClick={handleSave}>Save</button>
+                                    </div>
+                                    <div className="card-body">
+                                        <button type="submit" className="btn btn-secondary" onClick={handleCloseModal}>Cancel</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </ReactModal>
                 </React.Fragment>
             );
         }
+
+
+
+
         return (
             <div className="justify-content-center">
                 <div>
